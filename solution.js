e059434f3fdc34c68552c1ab740ec537af15f5d3
Cem <cemkgrbz@gmail.com>
2022-07-17 16:38:17 +0200

    modified solutions

--- solution.js
+++ solution.js
@@ -7,21 +7,17 @@ const sumIntFrom1ToN = (n) => {
     }
     return sum;
 }
-console.log(sumIntFrom1ToN(10))
+console.log(sumIntFrom1ToN(0))
 
 //2
 
-function sumOfCubes (x) {
-    let sum = 0; 
-    for (let i = 1; i <= x; i++) {
-        sum += Math.pow(i, 3);
-    }
-    return sum;
+function sumOfCubes (x,y,z) {
+    x = (x**3);
+    y = (y**3);
+    z = (z**3);
+    return x+y+z
 }
-console.log(sumOfCubes(5));
-
-
-// console.log(sumOfCubes(2, 3, 4))
+console.log(sumOfCubes(1,2,3)) //for empty parameters write a condition
 
 // const sumOfCubes = (x) => {
 //     let sum = 0;
@@ -40,27 +36,45 @@ function isStrStartOfWord (x, y) {
     } else {
         console.log(false);
     }
+
+    // if (y.search(x)=0) {
+    //     return true
+    // } else {
+    //     return false
+    // }
 }
-isStrStartOfWord('Ro', 'Napoli')
+isStrStartOfWord('pppppp', 'Napoli')
 
 //4
 
 function isLEQZero (n) {
     if (n <= 0) {
-        console.log(true);
+        // console.log(true)
+        return true
     } else {
-        console.log(false);
+        // console.log(false)
+        return false
     }
 }
-
-isLEQZero(-45)
+console.log(isLEQZero(-48))
 
 //5
 
 function countOccurrences (x, y) {
     console.log(x.split(y).length);
 }
-countOccurrences ('Count occurrencccccces', 'c')
+countOccurrences ('Count occurrences', 'c')
+
+// function countOccurrences2 (str, letter) {
+//     let counter = 0;
+//     for (let i = 0; i <str.length; i++){
+//         if(str[i]==letter){
+//             counter++;
+//         }
+//     }
+//     return 'The string have this letter for '+ counter + ' times';
+// }
+// console.log(countOccurrences2('xxxxxxxxyyy', 'y'))
 
 //6
 
@@ -71,7 +85,14 @@ function calcBaseToExponent (x, y) {
     };
     return expPower;
 };
-console.log(calcBaseToExponent(4,4))
+console.log(calcBaseToExponent(5,5))
+
+//or
+
+function xExponentY (x, y) {
+    return x**y 
+}
+console.log(xExponentY(5,5))
 
 //7
 
@@ -90,43 +111,74 @@ calcLifetimeSupply (32,2)
 //Bonus
 //a
 function calcLifetimeSupplyMaxAge (x, y, maxAge) {
-    console.log(`The snack company should provide you with ${((maxAge-x)*365)*y} units, until you are a ripe old age of 100. Happy snacking!`)
+    console.log(`The snack company should provide you with ${((maxAge-x)*365)*y} units, until you are a ripe old age of ${maxAge}. Happy snacking!`)
 }
 calcLifetimeSupplyMaxAge (32, 2, 70)
 
 //b
 function calcLifetimeSupplyFloating (x, y, maxAge) {
-    console.log(`The snack company should provide you with ${(((maxAge-x)*365)*y).toFixed(3)} units, until you are a ripe old age of 100. Happy snacking!`)
+    console.log(`The snack company should provide you with ${(((maxAge-x)*365)*y).toFixed(3)} units, until you are a ripe old age of ${maxAge}. Happy snacking!`)
 }
 calcLifetimeSupplyFloating (32, 1.67, 70)
 
 //9
 
+function isWaldoHere (str) {
+    let newStr = str.toLowerCase()
+    return newStr.includes('waldo')
+}
+
+console.log(isWaldoHere('I am Waldo'))
+
+
 //10
 
 function isEqualSlices (x,y,z) {
     if (y*z <= x) {
-        console.log(true)
+        // console.log(true)
+        return true
     } else {
-        console.log(false)
+        // console.log(false)
+        return false
     }
 }
-isEqualSlices (18, 6, 3)
+console.log(isEqualSlices (18, 6, 3))
 
 //11
 
 function isEqualNumXandO (n) {
     if (n.split('x').length == n.split('o').length) {
-        console.log(true)
+        // console.log(true)
+        return true
     } else {
-        console.log(false)
+        // console.log(false)
+        return false
     }
 }
-isEqualNumXandO('xo xo xo xo xx')
+console.log(isEqualNumXandO('xo xo xo xo xx'))
 
 //12
 
+// function isPrime(num) {
+//     if (num<=1) {
+//         return 'Not a prime'
+//     } else if (num > 2 && num % 2 === 0 && num % 3 === 0){
+//         return false
+//     } else { 
+//         for....
+//         // return 'Its a prime number' //else is optional here
+//     }
+// }
 
+// console.log(isPrime(9))
+
+function isPrime(num) {
+    for(let i=2; i<num; i++) {
+        if(num % i === 0) return 'not a prime'
+    }
+        return num>1
+}
+console.log(isPrime(12))
 
 //13 (in progress)
 
